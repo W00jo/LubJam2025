a96b3ec3e7fc67701740891ca2f2d9dbbb722019
rzabka1 <jturovska@gmail.com>
2025-01-25 18:52:34 +0100

    deaths_and_speed

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,5 +4,12 @@ var has_bullet: bool = true
 
 var has_shield: bool = true
 
-var dolphin_dead: bool = false
 var guy_dead: bool = false
+var dolphin_dead: bool = false
+
+var guy_speed = 350
+var dolphin_speed = 350
+
+
+func end_of_game():
+	print("END OF GAME")
