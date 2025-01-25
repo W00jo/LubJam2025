5ea63a6eb041bd89605b2ef1fd2a44155d754ea4
rzabka1 <jturovska@gmail.com>
2025-01-25 23:01:00 +0100

    updated_parallax

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var camera = $Camera2D
 @onready var side = $Side
 
-var speed = 150
+var speed = 300
 
 func _process(delta: float) -> void:
 	camera.position += camera.transform.x * speed * delta
