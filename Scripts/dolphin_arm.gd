7bd8c6a61c043fd009fdf5ddf837acccbfc0669a
rzabka1 <jturovska@gmail.com>
2025-01-25 15:13:14 +0100

    fixed_aiming

--- Scripts/dolphin_arm.gd
+++ Scripts/dolphin_arm.gd
@@ -18,14 +18,15 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.is_action_released("Shoot")):
 		aim_line.clear_points()
-		if Global.has_bullet == true:
-			var new_bullet = bullet.instantiate()
-			get_tree().root.add_child(new_bullet)
-			new_bullet.global_position = bullet_spawn.global_position
-			new_bullet.rotation = rotation
-			Global.has_bullet = false
+		#if Global.has_bullet == true:
+		var new_bullet = bullet.instantiate()
+		get_tree().root.add_child(new_bullet)
+		new_bullet.global_position = bullet_spawn.global_position
+		new_bullet.rotation = rotation
+		Global.has_bullet = false
 	
 	if event.is_action_pressed("Shoot"):
-		if Global.has_bullet == true:
+		#if Global.has_bullet == true:
 			aim_line.add_point(aim_line.position, 0)
-			aim_line.add_point(get_global_mouse_position()*10, 1)
+			var end_aim = Vector2(aim_line.position.x + 2000, aim_line.position.y)
+			aim_line.add_point(end_aim, 1)
