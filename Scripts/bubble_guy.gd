88f1351bdd9d0b27da5913606f616551e33c9ca2
Jakub <w00jcikjakub@gmail.com>
2025-01-25 17:59:24 +0100

    shield fix

--- Scripts/bubble_guy.gd
+++ Scripts/bubble_guy.gd
@@ -10,14 +10,8 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity * 0.97
 	move_and_slide()
 	
-<<<<<<< Updated upstream
-=======
-
-
-
-
 func _on_shield_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Bullet"):
 		print("Wykryty chłop. KŁUJ")
-		#Global.has_shield = falsepass # Replace with function body.
->>>>>>> Stashed changes
+		Global.has_shield = false
+		
--- Scripts/shield_bubble.gd
+++ Scripts/shield_bubble.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _on_collection_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("BubbleGuy"):
+	if (body.is_in_group("BubbleGuy") && Global.has_shield == false):
+		print("URATOWANY")
 		Global.has_shield = true
 		queue_free()
 		
