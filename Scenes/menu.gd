eed519071e9615215c22e73c5e815a280a42b164
rzabka1 <jturovska@gmail.com>
2025-01-26 08:45:24 +0100

    menu_instructions_update

---
+++ Scenes/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var anim = $AnimationPlayer
+@onready var canvas = $CanvasLayer
+@onready var instructions = get_tree().root.get_node('Game/InstructionLayer')
+@onready var menu = get_tree().root.get_node('Game/MenuLayer')
+
+func _ready() -> void:
+	get_tree().paused = true
+	anim.play("splashscreen_fadeout")
+
+func _on_play_pressed() -> void:
+	instructions.visible = true
+	menu.queue_free()
+
+func _on_credits_pressed() -> void:
+	pass
+	
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func remove_splash():
+	canvas.queue_free()
