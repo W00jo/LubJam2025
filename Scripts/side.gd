e42b9763fdf9e178c41027cb2b24400dfe4b83fe
Jakub <w00jcikjakub@gmail.com>
2025-01-27 12:55:58 +0100

    v. 1.0.1
    hell yeah

--- Scripts/side.gd
+++ Scripts/side.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Dolphin"):
 		Global.dolphin_dead = true
 		Global.end_of_game()
+		Global.guy_win()
 		print("Dolphin dead")
 	if body.is_in_group("BubbleGuy"):
 		Global.guy_dead = true
 		Global.end_of_game()
+		Global.dolphin_win()
 		print("Guy dead")
